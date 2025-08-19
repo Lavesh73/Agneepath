7fe29497605264d80c110e1950bbd7ed3bb02cd8
Lavesh73 <jangidlavesh733@gmail.com>
2025-08-20 01:49:37 +0530

    Add files via upload

---
+++ backend/src/lib/db.js
@@ -0,0 +1,14 @@
+import moongoose from 'mongoose';
+
+export const connectDB = async () => {
+    try {
+        await moongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected successfully');
+    } catch (error) {
+        console.error('MongoDB connection failed:', error);
+        process.exit(1); // Exit the process with failure
+    }
+}
